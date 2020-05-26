b422570fb8c388ce2c040c4579cf0ad15db6c72c
ofir sasonov <ofirsasonov@mail.tau.ac.il>
2020-05-26 13:21:59 +0300

    update pass2

--- ConsoleApplication1/ConsoleApplication1/passtwo.c
+++ ConsoleApplication1/ConsoleApplication1/passtwo.c
@@ -8,15 +8,15 @@
 void passTwoMain(int argc, char* argv[])
 {
 	char firstString[5] = { 0 };
-	char immBefore[4] = { 0 }; //last spot for \0
-	char immAfter[3] = { 0 };
+	char immBefore[51] = { 0 }; //last spot for \0
+	char cl = '0';
 	int adress = 0;
+	int count = 0;
 	HashTable* table = NULL;
 	table = passoneMain();
 	//in args: test.txt write.txt memin.txt
 	FILE *f1 = fopen(argv[2], "r");
 	FILE *memin = fopen(argv[3], "w");
-	immBefore[4] = '\0';
 
 	if (f1 == NULL || memin == NULL)
 	{
@@ -26,25 +26,41 @@ void passTwoMain(int argc, char* argv[])
 	}
 	while (!feof(f1))
 	{
-		fscanf(f1, firstString);
-		fscanf(f1, "%s",immBefore); //take original imm
+		fscanf(f1, firstString); //string untill space opcode-rd-rs-rt
+		//fscanf(f1, "%s",immBefore); //take original imm
+		//HOW TO PUT \0 IN THE RIGHT SPOT?
+		count = 0;
+		while (cl = getc(f1) != '\n')
+		{
+			if (cl != ' ') //if it continues after the fscanf including the space
+			{
+				immBefore[count] = cl;
+				count++;
+			}
+		}
+		immBefore[count] = '\0';
 
 		fprintf(memin, "%s", firstString); //write to memin all the line but the imm
 
 		//check if label or number
-		//if label(\0) find code in table and put in immAfter
+		//if label returns code from table else returns -1
 		adress=get_adress_from_label(table, immBefore);
 		
-		//if number (adress=-1) immAfter=immBefore
 		if (adress == -1)
 		{
+			//if it was already a number it was in hexa
 			fprintf(memin, "%03s\n", immBefore); //only the first 3 chars? no \0
 		}
 		else
 		{
-			//adress from int to string -> inmmAfter
+			//adress is int, write as hexa
 			fprintf(memin, "%03X\n", adress);
 		}
 
 	}
+
+	fclose(f1);
+	fclose(memin);
+
+	free_table(table);
 }
