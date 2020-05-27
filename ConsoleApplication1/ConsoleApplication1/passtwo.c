6fb54aaf86252f858db832966e1a4ca8de57bf35
ofir sasonov <ofirsasonov@mail.tau.ac.il>
2020-05-27 14:27:45 +0300

    pass two working!

--- ConsoleApplication1/ConsoleApplication1/passtwo.c
+++ ConsoleApplication1/ConsoleApplication1/passtwo.c
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 #include "common.h"
 
-void passTwoMain(int argc, char* argv[])
+void main(int argc, char* argv[])
 {
-	char firstString[5] = { 0 };
+	char firstString[6] = { 0 };
 	char immBefore[51] = { 0 }; //last spot for \0
 	char cl = '0';
 	int adress = 0;
@@ -26,36 +26,22 @@ void passTwoMain(int argc, char* argv[])
 	}
 	while (!feof(f1))
 	{
-		fscanf(f1, "%s", firstString); //string untill space opcode-rd-rs-rt
-		//fscanf(f1, "%s",immBefore); //take original imm
-		count = 0;
-		while ((cl = getc(f1)) != '\n') //get imm - as a string (\0)
-		{
-			if (cl != ' ') //if it continues after the fscanf including the space
-			{
-				immBefore[count] = cl;
-				count++;
-			}
-		}
-		immBefore[count] = '\0';
+		fscanf(f1, "%s %s", firstString, immBefore); //string untill space opcode-rd-rs-rt and imm
+		//printf("%s %s\n", firstString, immBefore);
 
-		fprintf(memin, "%s", firstString); //write to memin all the line but the imm
-
-		//check if label or number
 		//if label - returns code from table, else returns -1
 		adress = get_adress_from_label(table, immBefore);
 
 		if (adress == -1) //if number
 		{
 			//if it was already a number it was in hexa
-			fprintf(memin, "%03s\n", immBefore); //only the first 3 chars? no \0
+			fprintf(memin, "%s%03s\n",firstString, immBefore); 
 		}
 		else //if label
 		{
 			//adress is int, write as hexa
-			fprintf(memin, "%03X\n", adress);
+			fprintf(memin, "%s%03X\n",firstString, adress);
 		}
-
 	}
 
 	fclose(f1);
